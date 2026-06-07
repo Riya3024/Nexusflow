36ff0cc0dfa3cd45a54935c412e8e8226656ca22
Riya3024 <riyapanditstm@gmail.com>
2026-06-07 12:23:11 +0530

    final submission

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate, Link } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = import.meta.env.VITE_API_URL;
+
 
 console.log("API_URL =", API_URL);
 
